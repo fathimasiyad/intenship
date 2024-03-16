69a3a50d356da7d84b0350a2e68c4b080f5e19a2
fathimasiyad <163624568+fathimasiyad@users.noreply.github.com>
2024-03-16 11:05:28 +0530

    Add files via upload

---
+++ python_internship/guii.py
@@ -0,0 +1,59 @@
+# from tkinter import*
+# root=Tk()
+# root.mainloop()
+
+# from tkinter import*
+
+
+# run=Tk()
+# run.mainloop()
+
+# from tkinter import*
+# root=Tk()
+# root.title("welcome to python lobby")
+# root.geometry("300x200")
+# root.mainloop()
+# from tkinter import*
+# root=Tk()
+# root.title("welcome to python lobby")
+# root.geometry("200x250")
+# lbl=Label(root,text="we are python lobby-ian")
+# lbl.pack()
+# root.mainloop()
+
+# from tkinter import*
+# root=Tk()
+# root.title("welcome to python loop")
+# root.geometry("250x250")
+# lbl=Label(root,text='my name is affin',font=("helvetica",12),fg='white',bg='black')
+# lbl.pack()
+
+# root.mainloop()
+
+# from tkinter import*
+# root=Tk()
+
+# root.title("welcome to python lobby")
+# def clicked():
+#     print(" am iclicked")
+# btn=Button(root,text="click me",command=clicked)
+# btn.pack()
+# root.geometry('250x200')
+# root.mainloop()
+
+# from tkinter import*
+# root=Tk()
+# root.title("welcom to python lobby")
+# root.geometry("250x200")
+# entry=Entry(root,bg="yellow",fg="red",bd=5)
+# entry.pack()
+# root.mainloop()
+
+
+from tkinter import*
+root=Tk()
+root.title("welcome to python lobby")
+root.geometry("250x200")
+entry=Entry(root,bg="yellow",fg="red",bd=5)
+entry.pack()
+root.mainloop()
